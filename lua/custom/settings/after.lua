c024455a458c1b9491e3bf6fd47341fbcc3d1619
Qian Feng <qian.feng@hotmail.com>
2026-01-19 11:26:55 +0800

    Enable clangd

--- lua/custom/settings/after.lua
+++ lua/custom/settings/after.lua
@@ -1,3 +1,5 @@
 vim.keymap.set('n', '<leader>tt', ':FloatermToggle<CR>')
 vim.keymap.set('n', '<F12>', ':FloatermToggle<CR>')
 vim.keymap.set('t', '<F12>', '<C-\\><C-n>:FloatermToggle<CR>')
+
+vim.lsp.enable { 'clangd' }
