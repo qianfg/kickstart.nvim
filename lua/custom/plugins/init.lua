34bb0fdf698ea30f9edc929d62a363680a507937
Qian Feng <qian.feng@hotmail.com>
2026-01-20 16:17:52 +0800

    mrcjkb/rustaceanvim, rafamadriz/friendly-snippets, use super-tab preset for blink and some keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,4 +8,18 @@ return {
     cmd = 'FloatermToggle',
   },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+      local ls = require 'luasnip'
+      -- Extend the 'cpp' filetype to also include 'c' snippets
+      ls.filetype_extend('cpp', { 'c' })
+    end,
+  },
 }
--- lua/custom/settings/after.lua
+++ lua/custom/settings/after.lua
@@ -2,4 +2,8 @@ vim.keymap.set('n', '<leader>tt', ':FloatermToggle<CR>')
 vim.keymap.set('n', '<F12>', ':FloatermToggle<CR>')
 vim.keymap.set('t', '<F12>', '<C-\\><C-n>:FloatermToggle<CR>')
 
-vim.lsp.enable { 'clangd' }
+vim.lsp.enable { 'clangd', 'gopls' }
+-- vim.keymap.set('n', '<leader>ec', ':tabnew $MYVIMRC<cr>', { desc = 'Open Neovim configuration file' })
+vim.keymap.set('n', '<leader>ec', function()
+  vim.cmd.tabedit(vim.fn.stdpath 'config')
+end, { desc = 'Open Neovim configuration file' })
